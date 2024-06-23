61fa2aa2b3a66bcfc6a26c5b032c87d5318ad15f
Stephen Zhou <hi@hyoban.cc>
2024-06-23 09:57:41 +0800

    chore: workaround

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { writeFileSync } from 'node:fs'
-import path from 'node:path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
@@ -7,7 +8,7 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(import.meta.dirname, './src/index.ts'),
+      entry: resolve(dirname(fileURLToPath(import.meta.url)), './src/index.ts'),
       formats: ['es', 'cjs'],
       fileName: 'index',
     },

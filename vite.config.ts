8861998bc7093e3012a2d6692586444e2ec6999a
Stephen Zhou <hi@hyoban.cc>
2024-06-23 09:59:30 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { writeFileSync } from 'node:fs'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
@@ -8,17 +6,19 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(dirname(fileURLToPath(import.meta.url)), './src/index.ts'),
+      entry: './src/index.ts',
       formats: ['es', 'cjs'],
       fileName: 'index',
     },
     minify: false,
   },
-  plugins: [dts({
-    include: ['./src/index.ts'],
-    beforeWriteFile: (filePath, content) => {
-      writeFileSync(filePath.replace('.d.ts', '.d.cts'), content)
-      return { filePath, content }
-    },
-  })],
+  plugins: [
+    dts({
+      include: ['./src/index.ts'],
+      beforeWriteFile: (filePath, content) => {
+        writeFileSync(filePath.replace('.d.ts', '.d.cts'), content)
+        return { filePath, content }
+      },
+    }),
+  ],
 })
